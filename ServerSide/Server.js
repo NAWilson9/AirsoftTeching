d23409be83d2b1276d1e959ecb895d38a5dd19db
Nick Wilson <nawilson9@gmail.com>
2015-03-03 22:16:11 -0600

    Added Express/Test

--- ServerSide/Server.js
+++ ServerSide/Server.js
@@ -1,4 +1,20 @@
 /**
  * Created by Nick on 3/3/2015.
  */
-//TEST WOOT
+//Link dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+
+//Create server
+var server = express();
+
+//Server Setup
+server.use(bodyParser.json());
+server.use(express.static('../ClientSide/'));
+
+//Start server
+server.listen(1337, function(err) {
+    console.log('AirsoftTeching server running on port ' + 1337);
+});
+
+//AJAX Calls
