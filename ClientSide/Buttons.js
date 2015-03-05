c6082a75994ddc22df237c6921f404b8513898ac
Nick Wilson <nawilson9@gmail.com>
2015-03-04 21:55:18 -0600

    - Updated Index.HTML metadata and buttons - Created BatteryPage - Included fs on server - Added server routes for /getHomePage and /getBatteryPage

--- ClientSide/Buttons.js
+++ ClientSide/Buttons.js
@@ -10,12 +10,12 @@ function httpGet(url, callback)
 {
     var request = new XMLHttpRequest();
     request.onreadystatechange = function() {
-        if(request.readyState == 4 && request.status == 200) {
+        if(request.readyState == 4 && request.status == 200){
             callback(request.responseText);
         }
     };
     request.open("GET", url, true);
-    request.send();
+    request.send(null);
 }
 
 //Generic get page method
